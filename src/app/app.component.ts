efe658b67a1b1d186d85f7e972cba824095f792a
Tobias Keller <tbskllr@gmail.com>
2020-01-08 23:18:50 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  template: `
+    <div>
+      <app-simple-form></app-simple-form>
+    </div>
+  `,
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "Hello Angular";
 }
